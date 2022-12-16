552314f1cd2c0ffd218d9581ce80f225dadf6f44
bryaneriksson <bryaneriksson@uvic.ca>
2022-12-15 22:49:30 -0800

    Added link to posts

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 export default function Posts() {
   return (
-    <h1> Posts Page </h1>
+    <>
+        <h1> Posts Page </h1>
+        <Link href="/"> Back to home! </Link>
+    </>
   )
 }
